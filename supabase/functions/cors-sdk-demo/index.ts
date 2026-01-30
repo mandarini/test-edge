d39f216c2dff266ed14d95316cd0b140cdf309b2
Katerina Skroumpelou <sk.katherine@gmail.com>
2026-01-30 16:22:02 +0200

    fixed

--- supabase/functions/cors-sdk-demo/index.ts
+++ supabase/functions/cors-sdk-demo/index.ts
@@ -232,7 +232,7 @@ curl -i 'http://127.0.0.1:54321/functions/v1/cors-sdk-demo?scenario=custom-origi
 
 
 # With credentials
-curl -i 'http://127.0.0.1:54321/functions/v1/cors-sdk-demo?scenario=with-credentials' \
+curl -i 'http://127.0.0.1:54321/functions/v1/cors-sdk-demo?scenario=custom-origin' \
   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 
 # Additional headers
